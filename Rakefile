8c3c518c16cd73368e7f564ef943eb2b42449bc2
Laurent B <lbnetid+gh@gmail.com>
2016-12-11 15:29:05 +0100

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,4 @@
+require 'bundler/gem_tasks'
+task :default => :spec
+
+require 'easy_app_helper/tasks'
---
+++ lib/signature_generator.rb
@@ -0,0 +1,5 @@
+require "signature_generator/version"
+
+module SignatureGenerator
+  # Your code goes here...
+end
