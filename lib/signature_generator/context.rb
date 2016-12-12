99ac45217885800e3fc4ad431908d0e5887d257a
Laurent B <lbnetid+gh@gmail.com>
2016-12-12 01:07:44 +0100

    Fix

--- lib/signature_generator/context.rb
+++ lib/signature_generator/context.rb
@@ -7,6 +7,10 @@ module SignatureGenerator
       @internal_hash = hash
     end
 
+    def []=(key,val)
+      internal_hash[key] = val
+    end
+
     def method_missing(*args)
       method_name = args.shift
       return internal_hash[method_name] if internal_hash[method_name]
